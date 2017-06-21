1f431e0b1a12f0d6d74850c9fbdcc74f6e78eff6
balov <alexandre.balov@gmail.com>
2017-06-21 11:05:51 -0700

    Joker 50:50 implemnted

--- WhoWantsToBeAMillionaire/MainWindow.xaml.cs
+++ WhoWantsToBeAMillionaire/MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using WhoWantsToBeAMillionaire.Helpers;
 using WhoWantsToBeAMillionaire.Questions;
 
 namespace WhoWantsToBeAMillionaire
@@ -12,20 +11,19 @@ namespace WhoWantsToBeAMillionaire
     /// </summary>
     public partial class MainWindow : Window
     {
-        int counterRounds;
+        private int counterRounds;
 
         public MainWindow()
         {
+            var currentQuestion = GetQuestion.GetEasyQuestion(counterRounds);
             InitializeComponent();
-            CallEasyQuestions();
+            CallEasyQuestions(currentQuestion);
         }
-
 
         private void AnswerOne_OnClick(object sender, RoutedEventArgs e)
         {
             CheckForAnswer(AnswerOne);
         }
-
 
         private void AnswerTwo_OnClick(object sender, RoutedEventArgs e)
         {
@@ -44,7 +42,27 @@ namespace WhoWantsToBeAMillionaire
 
         private void JokerOne_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            if (counterRounds < 5)
+            {
+                var currentQuestion = GetQuestion.GetEasyQuestion(counterRounds);
+                currentQuestion.WrongAnswers[0] = "Wrong";
+                currentQuestion.WrongAnswers[1] = "Wrong";
+                CallEasyQuestions(currentQuestion);
+            }
+            else if (counterRounds >= 5 && counterRounds < 10)
+            {
+                var currentQuestion = GetQuestion.GetMediumQuestion(counterRounds - 5);
+                currentQuestion.WrongAnswers[0] = "Wrong";
+                currentQuestion.WrongAnswers[1] = "Wrong";
+                CallMediumQuestions(currentQuestion);
+            }
+            else
+            {
+                var currentQuestion = GetQuestion.GetHardQuestion(counterRounds - 10);
+                currentQuestion.WrongAnswers[0] = "Wrong";
+                currentQuestion.WrongAnswers[1] = "Wrong";
+                CallHardQuestions(currentQuestion);
+            }
         }
 
         private void JokerTwo_OnClick(object sender, RoutedEventArgs e)
@@ -56,48 +74,17 @@ namespace WhoWantsToBeAMillionaire
             throw new NotImplementedException();
         }
 
-        private void CallEasyQuestions()
+        private void CallEasyQuestions(Question Question)
         {
-            var Question = GetQuestion.GetEasyQuestion(counterRounds);
+            //var Question = GetQuestion.GetEasyQuestion(counterRounds);
             QuestionToAsk.Text = Question.QuestionBody;
             var wrongAnswr = Question.WrongAnswers.Take(3);
             int random = DateTime.Now.Second;
-            if (random%4==0)
-            {
-                AnswerOne.Content = Question.Answer;
-                AnswerTwo.Content = wrongAnswr.ElementAt(0);
-                AnswerThree.Content = wrongAnswr.ElementAt(1);
-                AnswerFour.Content = wrongAnswr.ElementAt(2);
-            }
-            else if (random % 4 == 1)
-            {
-                AnswerOne.Content = wrongAnswr.ElementAt(0);
-                AnswerTwo.Content = Question.Answer;
-                AnswerThree.Content = wrongAnswr.ElementAt(1);
-                AnswerFour.Content = wrongAnswr.ElementAt(2);
-            }
-            else if (random % 4 == 2)
-            {
-                AnswerOne.Content = wrongAnswr.ElementAt(0);
-                AnswerTwo.Content = wrongAnswr.ElementAt(1);
-                AnswerThree.Content = Question.Answer;
-                AnswerFour.Content = wrongAnswr.ElementAt(2);
-            }
-            else
-            {
-                AnswerOne.Content = wrongAnswr.ElementAt(2);
-                AnswerTwo.Content = wrongAnswr.ElementAt(0);
-                AnswerThree.Content = wrongAnswr.ElementAt(1);
-                AnswerFour.Content = Question.Answer;
-            }
-        }
 
-        private void CallMediumQuestions()
-        {
-            var Question = GetQuestion.GetMediumQuestion(counterRounds - 5);
-            QuestionToAsk.Text = Question.QuestionBody;
-            var wrongAnswr = Question.WrongAnswers.Take(3);
-            int random = DateTime.Now.Second;
+            JokerOne.Content = "Joker 50:50";
+            JokerTwo.Content = "Phone a Friend";
+            JokerThree.Content = "Ask the Audience";
+
             if (random % 4 == 0)
             {
                 AnswerOne.Content = Question.Answer;
@@ -128,12 +115,17 @@ namespace WhoWantsToBeAMillionaire
             }
         }
 
-        private void CallHardQuestions()
+        private void CallMediumQuestions(Question Question)
         {
-            var Question = GetQuestion.GetHardQuestion(counterRounds - 10);
+            //var Question = GetQuestion.GetMediumQuestion(counterRounds - 5);
             QuestionToAsk.Text = Question.QuestionBody;
             var wrongAnswr = Question.WrongAnswers.Take(3);
             int random = DateTime.Now.Second;
+
+            JokerOne.Content = "Joker 50:50";
+            JokerTwo.Content = "Phone a Friend";
+            JokerThree.Content = "Ask the Audience";
+
             if (random % 4 == 0)
             {
                 AnswerOne.Content = Question.Answer;
@@ -164,36 +156,79 @@ namespace WhoWantsToBeAMillionaire
             }
         }
 
+        private void CallHardQuestions(Question Question)
+        {
+            //var Question = GetQuestion.GetHardQuestion(counterRounds - 10);
+            QuestionToAsk.Text = Question.QuestionBody;
+            var wrongAnswr = Question.WrongAnswers.Take(3);
+            int random = DateTime.Now.Second;
+
+            JokerOne.Content = "Joker 50:50";
+            JokerTwo.Content = "Phone a Friend";
+            JokerThree.Content = "Ask the Audience";
+
+            if (random % 4 == 0)
+            {
+                AnswerOne.Content = Question.Answer;
+                AnswerTwo.Content = wrongAnswr.ElementAt(0);
+                AnswerThree.Content = wrongAnswr.ElementAt(1);
+                AnswerFour.Content = wrongAnswr.ElementAt(2);
+            }
+            else if (random % 4 == 1)
+            {
+                AnswerOne.Content = wrongAnswr.ElementAt(0);
+                AnswerTwo.Content = Question.Answer;
+                AnswerThree.Content = wrongAnswr.ElementAt(1);
+                AnswerFour.Content = wrongAnswr.ElementAt(2);
+            }
+            else if (random % 4 == 2)
+            {
+                AnswerOne.Content = wrongAnswr.ElementAt(0);
+                AnswerTwo.Content = wrongAnswr.ElementAt(1);
+                AnswerThree.Content = Question.Answer;
+                AnswerFour.Content = wrongAnswr.ElementAt(2);
+            }
+            else
+            {
+                AnswerOne.Content = wrongAnswr.ElementAt(2);
+                AnswerTwo.Content = wrongAnswr.ElementAt(0);
+                AnswerThree.Content = wrongAnswr.ElementAt(1);
+                AnswerFour.Content = Question.Answer;
+            }
+        }
 
         private void CheckForAnswer(Button Answer)
         {
-            bool check ;
+            bool check;
             if (counterRounds < 5)
             {
                 check = Answer.Content == GetQuestion.GetEasyQuestion(counterRounds).Answer;
             }
             else if (counterRounds >= 5 && counterRounds < 10)
             {
-                check = Answer.Content == GetQuestion.GetMediumQuestion(counterRounds-5).Answer;
+                check = Answer.Content == GetQuestion.GetMediumQuestion(counterRounds - 5).Answer;
             }
             else
             {
-                check = Answer.Content == GetQuestion.GetHardQuestion(counterRounds-10).Answer;
+                check = Answer.Content == GetQuestion.GetHardQuestion(counterRounds - 10).Answer;
             }
             if (check)
             {
                 counterRounds++;
                 if (counterRounds >= 5 && counterRounds < 10)
                 {
-                    CallMediumQuestions();
+                    var currentQuestion = GetQuestion.GetMediumQuestion(counterRounds - 5);
+                    CallMediumQuestions(currentQuestion);
                 }
                 else if (counterRounds < 5)
                 {
-                    CallEasyQuestions();
+                    var currentQuestion = GetQuestion.GetEasyQuestion(counterRounds);
+                    CallEasyQuestions(currentQuestion);
                 }
                 else if (counterRounds >= 10 && counterRounds < 15)
                 {
-                    CallHardQuestions();
+                    var currentQuestion = GetQuestion.GetHardQuestion(counterRounds - 10);
+                    CallHardQuestions(currentQuestion);
                 }
                 else
                 {
@@ -204,7 +239,8 @@ namespace WhoWantsToBeAMillionaire
             {
                 MessageBox.Show("WrongAnswer");
                 counterRounds = 0;
-                CallEasyQuestions();
+                var currentQuestion = GetQuestion.GetEasyQuestion(counterRounds);
+                CallEasyQuestions(currentQuestion);
             }
         }
     }
